8530f5731c0b78b23a4cb670edb88b70170a7a43
WoldemarK <kovtynov.vladimir@gmail.com>
2023-02-13 22:37:01 +0300

    create class Person

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "person")
 public class Person {
