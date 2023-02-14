0fcabac516f654720c965cd04d103de7d930f182
WoldemarK <kovtynov.vladimir@gmail.com>
2023-02-14 18:22:24 +0300

    add method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,58 @@
 package org.example;
 
+import org.example.model.Item;
 import org.example.model.Person;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
 
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+            Item item = new Item("new ITEM", person);
+
+            person.getItems().add(item);
+
+            session.save(item);
+
+            Person newPerson = new Person("Test Person create", 20);
+
+            Item newItem = new Item("Test new Item", newPerson);
+
+            newPerson.setItems(new ArrayList<>(Collections.singletonList(newItem)));
+
+            session.save(newPerson);
+            session.save(newItem);
+
+            session.getTransaction().commit();
+//            Person person = session.get(Person.class, 1);
+//            System.out.println(person);
+//
+//            List<Item> list = person.getItems();
+//            System.out.println(list);
+//
+//            Item item = session.get(Item.class, 3);
+//            System.out.println(item);
+//            Person p = item.getOwner();
+//            System.out.println(p);
+
+
+        }
 
 //        try (sessionFactory) {
 //            Session session = sessionFactory.getCurrentSession();
@@ -55,13 +95,13 @@ public class App {
 //
 //            session.getTransaction().commit();
 //        }
-        try (sessionFactory) {
-            Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            List<Person>people = session.createQuery("from Person ").getResultList();
-            for (Person person : people)
-                System.out.println(person);
-            session.getTransaction().commit();
-        }
+//        try (sessionFactory) {
+//            Session session = sessionFactory.getCurrentSession();
+//            session.beginTransaction();
+//            List<Person>people = session.createQuery("from Person").getResultList();
+//            for (Person person : people)
+//                System.out.println(person);
+//            session.getTransaction().commit();
+//        }
     }
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,16 +1,13 @@
 package org.example.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
+import java.util.List;
+
+
 @Entity
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Table(name = "person")
 public class Person {
     @Id
@@ -22,4 +19,58 @@ public class Person {
 
     @Column(name = "age")
     private int age;
+
+    @OneToMany(mappedBy = "owner",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    private List<Item> items;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
